80e7f5f2c4854104181322e5d27bc1b9a3d1f1e4
Ma <562600828@qq.com>
2012-11-13 22:39:29 +0800

    消息模块
    消息模块的

--- Lib/Home/Action/MessageAction.class.php
+++ Lib/Home/Action/MessageAction.class.php
@@ -11,15 +11,56 @@ class MessageAction extends Action {
 
 	public function __construct() {
 		if (!USER_ID) {
-			$this->error('请先登录！');
+			$this->error('请先登录！', U('Login/index'));
 		}
 	}
 
 	//纸条盒子首页
 	public function index() {
+		//首先获取最近联系人10个
+		$temp = $this->_param('linkman');
+		$userId = USER_ID;
+		$messageObj = D('Message');
+		$userObj = M('User');
+		$linkman = $messageObj->getLinkman($userId);
+		foreach ($linkman as &$value) {
+			$linkID = $value['sender'];
+			if ($linkID == $userId)
+				$linkID = $value['receiver'];
+			$value['lastMessage'] = $messageObj->messagesList($linkID, $userId, 1);
+			$value['linkmanId'] = $linkID;
+			$value['linkman'] = $userObj->where(array('id' => $linkID))->find();
+		}
 		
+
+		$temp || $temp = reset($linkman)['receiver'];
+
+		if ($temp == $userId) {
+			$temp = reset($linkman)['sender'];
+		}
+
+		$listMessage['messages'] = $messageObj->messagesList($temp, $userId, 4);
+		$listMessage['mine'] = $userObj->where(array('id' => $userId))->find();
+		$listMessage['linkman'] = $userObj->where(array('id' => $temp))->find();
+		$this->assign('listMessage', $listMessage);
+		$this->assign('linkman', $linkman);
+		$this->display();
+	}
+
+	/**
+	 * 
+	 */
+	public function sendMessage() {
+		$receiver = $this->_post('receiver');
+		$content = $this->_post('content');
+		$sender = USER_ID;
+		$r = D('Message')->send($content, $receiver, $sender);
+		if ($r) {
+			$this->success('发送成功！');
+		} else {
+			$this->error('发送失败！');
+		}
 	}
 
 }
-
 ?>
--- Lib/Home/Model/MessageModel.class.php
+++ Lib/Home/Model/MessageModel.class.php
@@ -23,14 +23,50 @@ class MessageModel extends Model {
 		$NUM = substr($sender + $receiver, -1);
 		$rM = M('Message_' . $NUM)->add(array('sender' => $sender, 'receiver' => $receiver, 'content' => $content, 'date' => time()));
 		//写入联系人表
-		$linkmanObj = D('Linkman');
-		$rL = $linkmanObj->save(array('receiver' => $receiver, 'sender' => $sender, 'date' => time()));
+		$linkmanObj = M('Linkman');
+		//关于联系人，暂时没有想到好的分表方案，暂时先这么写着
+		$rL = $linkmanObj->where("(receiver = $receiver AND sender = $sender) OR (receiver = $sender AND sender = $receiver) ")->save(array('date' => time()));
 		$rL || $rL = $linkmanObj->add(array('receiver' => $receiver, 'sender' => $sender, 'date' => time()));
 
 		if ($rM) {
-			return FALSE;
+			return TRUE;
 		}
-		return TRUE;
+		return FALSE;
+	}
+
+	/**
+	 * 获取最近联系人！
+	 * @todo 最近联系人，和好友的概念 要好好分析，最后取舍
+	 */
+	public function getLinkman($userId = FALSE) {
+		$userId || $userId = USER_ID;
+		$linkmanObj = M('Linkman');
+		$linkman = $linkmanObj->where(array('receiver' => $userId, 'sender' => $userId, '_logic' => 'OR'))->order('date desc')->limit(10)->select();
+		return $linkman;
+	}
+
+	/**
+	 * 最后一条消息预览
+	 * @param type $linkman 联系人Id
+	 * @param type $number 获取的消息数量
+	 * @param type $userId 用户的Id
+	 */
+	public function messagesList($linkman, $userId, $number = 1) {
+		$userId || USER_ID;
+		$messageObj = M('Message_' . $this->getTable($linkman, $userId));
+		$messages = $messageObj->where(array('receiver' => $userId, 'sender' => $userId, '_logic' => 'OR'))->order('date desc')->limit($number)->select();
+		return array_reverse($messages);
+	}
+
+	/**
+	 * 处理分表问题
+	 * @param type $linkman
+	 * @param type $userId
+	 */
+	private function getTable($linkman, $userId = FALSE) {
+		$userId || $userId = USER_ID;
+		$NUM = substr($linkman + $userId, -1);
+		return $NUM;
 	}
 
 }
